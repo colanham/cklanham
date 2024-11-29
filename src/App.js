5a5330d9f26f1cd69a3e609b6ad053b678d30ff2
Cole Lanham <coleklanham@gmail.com>
2024-11-28 21:00:10 -0500

    first set up and important links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const SUBJECT_LINE = "Hey Cole, I would love to connect! \u{1F525}"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'> 
+      <div className='info-container'>
+        <div className='info'>
+          <h1>c. k. lanham - personal site</h1>
+          <h2>wannabe swe | digital minimalist | cultural heretic</h2>
+          <ul>
+            <li><a href='docs/colelanham_resume2024.pdf' target='_blank' rel="noreferrer">resume</a></li>
+            <li><a href={'mailto:coleklanham@gmail.com?subject=' + SUBJECT_LINE}>contact me</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className='bottom-container'>
+        <ul>
+          <li><a href='https://github.com/colanham' target='_blank' rel="noreferrer">github</a></li>
+          <li><a href='https://x.com/GoalLanham' target='_blank' rel="noreferrer">x</a></li>
+          <li><a href='https://linkedin.com/in/cole-lanham' target='_blank' rel="noreferrer">linkedin</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
